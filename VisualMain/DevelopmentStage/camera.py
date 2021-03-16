4616852f8250b9ad32b18c97956d517acaae1840
rk2097o <75034262+rk2097o@users.noreply.github.com>
2021-03-16 23:54:08 +0000

    Add files via upload
    Camera Testing and frame conversion

---
+++ VisualMain/DevelopmentStage/camera.py
@@ -0,0 +1,37 @@
+#Created By Ravi Kumar
+#Date 26/02/2020
+#Description: Camera and opencv testing and converting camera feed to grayscale and HSV
+#import cv2 libraries
+import cv2
+
+#setting USb camera input 
+cap = cv2.VideoCapture(0)
+#frame width
+cap.set(3, 640) 
+#frame height
+cap.set(4, 480)	
+#frame brightness
+cap.set(10,70) 
+
+while(True):
+	#Capturing image frame by frame
+	ret, frame = cap.read()
+	
+	#frame operations 
+	#coverting image captured in frame to grayscale and HSV
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	
+	
+	#Display all the resulting framse HSV, Greyscale and Color
+	cv2.imshow('hsv', hsv)
+	cv2.imshow('gray' ,gray)
+	cv2.imshow('color', frame)
+	
+	#Stop the program and close the resulting frame with key press 'Q'
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+#release the capture and close all windows 
+cap.release()
+cv2.destroyAllWindows()
