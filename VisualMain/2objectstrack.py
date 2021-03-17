982becbe1b2580939d043cc24f141f3659f4b789
rb6145m <rb6145m@gre.ac.uk>
2021-03-17 21:36:06 +0000

    Update 2objectstrack.py
    Updated variable names to enable scaling of tracked number of objects and  Fixed Co-ordinates not being printed when only 1 object is in frame.

--- VisualMain/2objectstrack.py
+++ VisualMain/2objectstrack.py
@@ -1,8 +1,8 @@
-#Created By Maher and Ross
-#Date: 06/03/2021
-#Description: Object colour detection and tracking using colour bounds with OpenCV & Python3 Using 2 Object colours.
-#**** BEWARE Software is unfinished and requires comments to be written & correct formatting of car object X & Y.*** 
-#Version:0.1 
+#Created By Maher, Ross and Ravi 
+#Date: 26/02/2021
+#Description: Tracking of 1 object and 1 Vehicle based on 2 different colours
+#Version:1.1 (Release as of 17/03/21)
+#Update 1.1:Fixed Co-ordinates not being printed when only 1 object is in frame & Updated comments
 
 # import required packages
 from collections import deque
@@ -10,19 +10,19 @@ import numpy as np
 import imutils
 import cv2
 
-#Construct the map position objety with type value x & y
+#Construct the definition to print object position value x & y
 def createobjectxandy (x, y):
     print ("Object at X = {0} and Y =  {1}".format(x, y))
 
-#Construct the map position objety with type value x & y
+#Construct the definition to print car position value x & y
 def carcreateobjectxandy (x, y):
     print ("Car at X = {0} and Y =  {1}".format(carx, cary))
 
 
 # Set upper and lower limits of the object to be tracked bounds 
 # To be generated from Output from BGRconvert.py see Github
-colourLower = (8, 100, 100)
-colourUpper = (28, 255, 255)
+object1Lower = (8, 100, 100)
+object1Upper = (28, 255, 255)
 carlower = (160, 100, 100)
 carupper = (180, 255, 255)
  
@@ -31,111 +31,112 @@ webcam = cv2.VideoCapture(0)
 
 # keep looping
 while True:
+
     # capture the current video frame
     (grabbed, vid) = webcam.read()
  
-    #convert the frame from BGR to HSV
-    hsvcolour = cv2.cvtColor(vid, cv2.COLOR_BGR2HSV)
+    #convert the frame from BGR to HSV for object 1 
+    obj1hsvcolour = cv2.cvtColor(vid, cv2.COLOR_BGR2HSV)
     
-    #for car track
+    #convert the frame from BGR to HSV for car object 
     carhsvcolour = cv2.cvtColor(vid, cv2.COLOR_BGR2HSV)
 
     # convert the mask for the colour specified by the colour bounds 
     # remove the outliers in the mask using erode and dilate
-    convertedmask = cv2.inRange(hsvcolour, colourLower, colourUpper)
+    convertedmask = cv2.inRange(obj1hsvcolour, object1Lower, object1Upper)
     convertedmask = cv2.erode(convertedmask, None, iterations=2)
     convertedmask = cv2.dilate(convertedmask, None, iterations=2)
-    
     carconvertedmask = cv2.inRange(carhsvcolour, carlower, carupper)
     carconvertedmask = cv2.erode(carconvertedmask, None, iterations=2)
     carconvertedmask = cv2.dilate(carconvertedmask, None, iterations=2)
 
-    # use cv2.findContours to locate contours
-    contours = cv2.findContours(convertedmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    # use cv2.findContours to locate contours of object1contours
+    obj1contours = cv2.findContours(convertedmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
 
-    # CAR use cv2.findContours to locate contours
+    # CAR use cv2.findContours to locate contours of carcontours
     carcontours = cv2.findContours(carconvertedmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
     
-    # initialize the center of tracked colour/object
-    contourcenter = None
-    # initialize the center of tracked colour/object
+    # initialize the center of tracked colours/objects
+    obj1contourcenter = None
     carcontourcenter = None
-    #print ('carcont', carcontours)
-    #print ('objcont', contours)
 
     # if statement looking for one contour is found in contour que
-    if len(contours) > 0 and len(carcontours) <= 0:
+    if len(obj1contours) > 0 and len(carcontours) <= 0:
 
         # Take the last contour from the contour que,
         # compute the size of the circle to be drawn
-        c = max(contours, key=cv2.contourArea)
+        c = max(obj1contours, key=cv2.contourArea)
         ((x, y), rad) = cv2.minEnclosingCircle(c)
         M = cv2.moments(c)
-        contourcenter = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        #print ("single object")
+        obj1contourcenter = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+	#If the largest circle's radius in mask is greater than 10px (Object1)  
         if rad > 10:
+
             # place circle around the contour point,
             # update the x and y of the the center point
             cv2.circle(vid, (int(x), int(y)), int(rad),
                 (255, 255, 255), 3)
-            cv2.circle(vid, contourcenter, 4, (10, 255, 255), -1)
-        
+            cv2.circle(vid, obj1contourcenter, 4, (10, 255, 255), -1)
 
-    elif len(contours) <= 0 and len(carcontours) > 0:
+	# print object coordinates
+        createobjectxandy(int(x), int(y))
+        
+	#ElseIf looking for array legnth of objects 
+    elif len(obj1contours) <= 0 and len(carcontours) > 0:
 
         # Take the last contour from the contour que,
         # compute the size of the circle to be drawn
         car = max(carcontours, key=cv2.contourArea)
         ((carx, cary), carrad) = cv2.minEnclosingCircle(car)
         Mcar = cv2.moments(car)
-        carcontourcenter  = (int(Mcar["m10"] / Mcar["m00"]), int(Mcar["m01"] / Mcar["m00"]))
-        #print ("single car")
+        carcontourcenter = (int(Mcar["m10"] / Mcar["m00"]), int(Mcar["m01"] / Mcar["m00"]))  
 
+	#If the largest circle's radius in mask is greater than 10px (CAR)  
         if  carrad > 10:
+
             # place circle around the contour point,
             # update the x and y of the the center point
             cv2.circle(vid, (int(carx), int(cary)), int(carrad),
                 (255, 255, 255), 3)
             cv2.circle(vid, carcontourcenter, 4, (10, 255, 255), -1)
 
-    elif  len(carcontours) > 0 and len(contours) > 0:
+	# print car coordinates
+        carcreateobjectxandy(int(carx), int(cary))
+
+	#ElseIf looking if array legnth of Both objects is > 0
+    elif  len(carcontours) > 0 and len(obj1contours) > 0:
+
         # Take the last contour from the contour que,
         # compute the size of the circle to be drawn
         car = max(carcontours, key=cv2.contourArea)
         ((carx, cary), carrad) = cv2.minEnclosingCircle(car)
         Mcar = cv2.moments(car)
         carcontourcenter = (int(Mcar["m10"] / Mcar["m00"]), int(Mcar["m01"] / Mcar["m00"]))
-        # Take the last contour from the contour que,
-        # compute the size of the circle to be drawn
-        c = max(contours, key=cv2.contourArea)
+        c = max(obj1contours, key=cv2.contourArea)
         ((x, y), rad) = cv2.minEnclosingCircle(c)
         M = cv2.moments(c)
-        contourcenter = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        #print ("double")
+        obj1contourcenter = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
+
+	#If the largest circle's radius in mask is greater than 10px (Both Objects)  
         if rad > 10 and carrad > 10:
+
 	    # place circle around the contour point,
             # update the x and y of the the center point
             cv2.circle(vid, (int(carx), int(cary)), int(carrad),
                 (255, 255, 255), 3)
             cv2.circle(vid, carcontourcenter, 4, (10, 255, 255), -1)
-            # place circle around the contour point,
-            # update the x and y of the the center point
             cv2.circle(vid, (int(x), int(y)), int(rad),
                 (255, 255, 255), 3)
-            cv2.circle(vid, contourcenter, 4, (10, 255, 255), -1)
+            cv2.circle(vid, obj1contourcenter, 4, (10, 255, 255), -1)
 
-	
-
-	# print center of circle coordinates
-        createobjectxandy(int(x), int(y))
-	# print center of circle coordinates
+	# print object1 and car coordinates
         carcreateobjectxandy(int(carx), int(cary))
+        createobjectxandy(int(x), int(y))
 
-    # update queue of x and y co-ordinates using deque command
-    deque([]).appendleft(contourcenter)
-
-    # update queue of x and y co-ordinates using deque command
+    # update queue of x and y co-ordinates and remove 1 entry using deque command
+    deque([]).appendleft(obj1contourcenter)
     deque([]).appendleft(carcontourcenter)
 
 
