b148df93e85ffb04068433fc1b04cc5784546298
da5905p <da5905p@greenwich.ac.uk>
2021-03-17 01:16:22 +0000

    Restored to original

--- Network/LAN/sendFile.sh
+++ Network/LAN/sendFile.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-scp /Users/davidansa/Projects/sendFile/send.txt pi@192.168.1.112:sendFolder -y et1mob1m #PATH MUST CHANGE FROM PI TO ANOTHER PI
+scp /Users/davidansa/Projects/sendFile/send.txt pi@192.168.1.112:sendFolder #PATH MUST CHANGE FROM PI TO ANOTHER PI
 sendFile.sh
-expect 'Password'
-send 'et1mob1m'
 scp /Users/davidansa/UoGACE/Network/LAN/data.csv pi@192.168.1.112:receiveFolder #Same as above
 echo Successfully Sent!
