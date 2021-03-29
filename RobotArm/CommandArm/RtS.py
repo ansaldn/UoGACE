85b99599e6872e165179f47c0f97ac4b533143c2
Goncalo <73899629+GonBit@users.noreply.github.com>
2021-03-29 11:43:10 +0100

    Update and rename RobotArm/RtS.py to RobotArm/CommandArm/RtS.py

--- RobotArm/CommandArm/RtS.py
+++ RobotArm/CommandArm/RtS.py
@@ -1,10 +1,11 @@
-import usb_arm
+#Robot to Storage script
+#Created by Goncalo
 
+import usb_arm
 arm = usb_arm.Arm()
+
 grabForward = [[usb_arm.ElbowDown,2],[usb_arm.WristDown, .5],[usb_arm.GripsClose],[usb_arm.ElbowUp,2.2],[usb_arm.WristUp,.6]]
 arm.doActions(grabForward)
+
 rotate = [[usb_arm.BaseCtrClockWise,7],[usb_arm.GripsOpen],[usb_arm.BaseClockWise,6.8]]
 arm.doActions(rotate)
-
-
-
