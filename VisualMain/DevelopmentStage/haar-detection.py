c41400ac2652522853cc1f7344383f4507928e6f
shahd-reja <73899750+shahd-reja@users.noreply.github.com>
2021-03-30 18:39:18 +0300

    Update haar-detection.py

--- VisualMain/DevelopmentStage/haar-detection.py
+++ VisualMain/DevelopmentStage/haar-detection.py
@@ -1,18 +1,18 @@
 #written by : SHAHD R A F ALDAIHANI
 import cv2
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+FC = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Read the input image
-#img = cv2.imread('test.png')
-cap = cv2.VideoCapture('test.mp4')
+#i= cv2.imread('t.png')
+cap = cv2.VideoCapture('t.mp4')
 
 while cap.isOpened():
     _, img = cap.read()
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    g = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    f = fC.detectMultiScale(g, 1.1, 4)
 
-    for (x, y , w ,h) in faces:
+    for (x, y , w ,h) in f:
         cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0 , 0), 3)
 
     # Display the output
